2172f19a578e7954bb3cc449002b5e12c7486f01
tom10ye <qiancheng0608@gmail.com>
2017-08-26 14:10:44 -0700

    Initial Commit

---
+++ SummerYelpMock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SummerYelpMock
+//
+//  Created by Cheng Qian on 8/26/17.
+//  Copyright © 2017 Cheng Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SummerYelpMock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SummerYelpMock
+//
+//  Created by Cheng Qian on 8/26/17.
+//  Copyright © 2017 Cheng Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
